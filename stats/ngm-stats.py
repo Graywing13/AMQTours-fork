961e5e331bdfb54b140ae72e94cc67f0404e861d
graywing13 <30674558+Graywing13@users.noreply.github.com>
2026-06-20 10:16:22 -0700

    patch: change chanting text display

--- stats/ngm-stats.py
+++ stats/ngm-stats.py
@@ -2483,7 +2483,7 @@ def pct_text(value):
     return "N/A" if value is None or pd.isna(value) else f"{value:.2%}"
 
 def pct_text_with_fraction(value, fraction_string):
-    return f"@ {pct_text(value)} ({fraction_string})"
+    return f"({pct_text(value)}, {fraction_string})"
 
 def number_text(value, _ignore=None):
     decimals = 2
